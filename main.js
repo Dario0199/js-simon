147e9529bd1840e3941270c4411d2fbe3fc787ed
Dario0122 <dario.di.cuia@gmail.com>
2021-11-10 17:25:36 +0100

    prompt if

--- main.js
+++ main.js
@@ -7,30 +7,43 @@ const container = document.querySelector('.container-number')
 const number = document.querySelector('.number')
 
 genRandNumb(1, 100);
-const array = []
+
+
 //generazione 5 numeri random
 
 function genRandNumb(min, max){
     const array = []
     console.log(array);
+
     let numbRand
     for(let i = 0; i < 5; i++){
         numbRand = Math.floor( Math.random() * (max - min + 1) )+ min;
         console.log(numbRand);
         array.push(numbRand)
         number.innerText = array
+       
     }
-    
 };
-
+//generazione prompt
 let time = 5;
-let insertNum
+const finalNumb = []
+
 setTimeout(() => {
     number.classList.add('d-none')
     for(let i = 0; i < 5; i++){
     
         insertNum = parseInt( prompt('inserisci uno per volta i numeri ') );
-        console.log('numeri prompt',insertNum);
+        
+        if(insertNum >100 || insertNum < 1 || isNaN(insertNum)){
+            alert('inserisci un valore da 1 a 100')
+            
+            insertNum = parseInt( prompt('inserisci uno per volta i numeri ') );
+        } 
+        console.log('numeri inseriti',insertNum);  
+        finalNumb.push(insertNum); 
     }
+    console.log('numeri finali', finalNumb);
+    
+    
 }, time * 1000)
 
